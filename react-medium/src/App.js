66f6ef2f28ca31b7916443f184d9129969a87758
Sanjay Saravanan <ssaravanan6384@altimetrik.com>
2021-04-23 14:59:31 +0530

    modified header

--- react-medium/src/App.js
+++ react-medium/src/App.js
@@ -21,21 +21,13 @@ function App() {
   ];
 
   return (
-    <div
-      style={{
-        margin: "50px",
-        width: "1000px",
-        overflowY: "auto",
-        height: "500px",
-        border: "1px solid",
-      }}
-    >
+    <div>
       <div
         style={{
           display: "flex",
           justifyContent: "space-between",
           width: "1000px",
-          marginBottom: "50px",
+          margin: "50px",
         }}
       >
         {data.map((item, i) => (
@@ -44,18 +36,28 @@ function App() {
           </div>
         ))}
       </div>
-      {data.map((item) => (
-        <div
-          style={{
-            height: "500px",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <h3>{item.name}</h3>
-        </div>
-      ))}
+      <div
+        style={{
+          margin: "50px",
+          width: "1000px",
+          overflowY: "auto",
+          height: "500px",
+          border: "1px solid",
+        }}
+      >
+        {data.map((item) => (
+          <div
+            style={{
+              height: "500px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <h3>{item.name}</h3>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
